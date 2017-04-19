b2cff415b871f30db479362e74a93ee5e7a083a9
James Feore <jjfeore@gmail.com>
2017-04-19 10:42:50 -0700

    Finished last TODO and stretch TODO

--- Zach-James/server.js
+++ Zach-James/server.js
@@ -10,18 +10,15 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static('./public'));
 
-// TODO: (STRETCH) Write a new route that will handle a request and send the new.html file back to the user
-
-//Test, remove me!
+app.get('/new', function(request, response) {
+  response.sendFile('public/new.html', {root: '.'});
+});
 
 app.post('/articles', bodyParser, function(request, response) {
-  // REVIEW: This route will receive a new article from the form page, new.html,
-  // and log that form data to the console. We will wire this up soon to actually
-  // write a record to our persistence layer!
   console.log(request.body);
   response.send('Record posted to server!!');
 })
 
 app.listen(PORT, function() {
-  // TODO: Log to the console a message that lets you know which port your server has started on
+  console.log(`Our server is running on port: ${PORT}`);
 });
