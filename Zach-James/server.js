b056ab1ad8e860145b9aaff532b1c09b63b8ecb7
Zachary Diehl <zachary.diehl@gmail.com>
2017-04-19 10:12:50 -0700

    complete first 3 TODO's

---
+++ Zach-James/server.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const express = require('express');
+
+const app = express();
+
+const bodyParser = require('body-parser').urlencoded();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static('./public'));
+
+// TODO: (STRETCH) Write a new route that will handle a request and send the new.html file back to the user
+
+
+app.post('/articles', bodyParser, function(request, response) {
+  // REVIEW: This route will receive a new article from the form page, new.html,
+  // and log that form data to the console. We will wire this up soon to actually
+  // write a record to our persistence layer!
+  console.log(request.body);
+  response.send('Record posted to server!!');
+})
+
+app.listen(PORT, function() {
+  // TODO: Log to the console a message that lets you know which port your server has started on
+});
